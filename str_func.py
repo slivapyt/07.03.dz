c5753767442f1703c8bcc37d821166e446699176
slivapy <faer2325@gmail.com>
2024-03-07 20:57:54 +0700

    работаем ребята

--- str_func.py
+++ str_func.py
@@ -1,6 +1,17 @@
-def func(text):
+test = input()
+
+
+def func_up(text):
+    """Капс"""
     return text.upper()
 
 
-test = input()
-print(func(test))
+print(func_up(test))
+
+
+def func_tit(text):
+    """Титульник"""
+    return text.title()
+
+
+print(func_tit(test))
