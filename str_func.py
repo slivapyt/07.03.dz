165f2073786b60cfab2ff47d8e5530abb4107e20
slivapy <faer2325@gmail.com>
2024-03-07 21:20:06 +0700

    ура

--- str_func.py
+++ str_func.py
@@ -1,7 +1,17 @@
-def func(text):
-    """возвращает заглавные"""
+test = input()
+
+
+def func_up(text):
+    """Капс"""
     return text.upper()
 
 
-test = input()
-print(func(test))
+print(func_up(test))
+
+
+def func_tit(text):
+    """Титульник"""
+    return text.title()
+
+
+print(func_tit(test))
